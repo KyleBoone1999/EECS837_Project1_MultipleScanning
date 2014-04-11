31be801c939cf7fba486107becef05823a60aef3
KyleBoone1999 <akassbb@yahoo.com>
2014-04-11 17:42:59 -0500

    Adds disjoint set class

--- proj1.py
+++ proj1.py
@@ -1,20 +1,72 @@
 #This is the main program for Project #1 EECS 837
 
+class DisjointDict:
+	def __init__(self,elementSetList):
+		self = dict()
+		for elementSet in elementSetList:
+			for case in elementSet.cases[1:-1]:
+				union(elementSet.cases[0], case)
+
+	def union(case1, case2):
+		root1 = find(case1)
+		root2 = find(case2)
+		self.update(root2,root1)
+
+	def find(case):
+		if(self[case] != case):
+			self.update(case, find(self[case]))
+		return self[case]
+
+class ElementalSet:
+	def __init__(self, case, sharedValue):
+		self.sharedValue = sharedValue
+		self.cases = list()
+		self.cases.append(case)
+
+class Partition:
+	def __init__(self):
+		self.elementalSets = list()
+	
+	def addToElementalSet(self, case, sharedValue):
+		for elementalSet in self.elementalSets:
+			if (sharedValue == elementalSet.sharedValue):
+				elementalSet.cases.append(case)
+		self.elementalSets.append(ElementalSet(case, sharedValue))
+	
+	def mergePartition(self, partition2)
+		disjointSet = DisjointDict(partition2)
+		for elementalSet in self.elementalSets:
+			for case1 in elementalSet.cases:#needs more work
+
+
 class Attribute:
-	def __init__(self, attrName, numericBoolean):
-		self.isNumeric = numericBoolean
+	def __init__(self, attrName):
 		self.name = attrName
 		self.numericValue = dict()
 		self.cutpoints = list()		
 		self.discreteValue = dict()
 
-	def addNumericValue(case, numValue, self):
-		if self.numericValue:
-			self.numericValue.update(case,numValue)
-		else:
-			self.numericValue = dict((case,numValue))
+	def addNumericValue(self, case, numValue):
+		self.numericValue.update(case,numValue)
+	
+	def fromNumericToDiscrete(self):
+		sortedPoints = sorted(self.cutpoints.append(sortedNumList[-1],sortedNumList[0]))
+		for case, numValue in self.numericValue.iteritems():
+			intervalStart = maxBelow(numValue,sortedPoints)
+			discValue = "%d..%d"%(sortedPoints[intervalStart],sortedPoints[intervalStart+1])
+			self.discreteValue.update(case,discValue)
 
-	#Create sorted list to calculate possible cut values, min and max
+	def maxBelow(value, sortedList):
+		midpoint = len(sortedList)/2
+		if (sortedList[midpoint] <= value and sortedList[midpoint+1] >=value):
+			return midpoint
+		if value > sortedList[midpoint]:
+			return maxLow(value,sortedList[midpoint:-1])
+		elif value < sortedList[midpoint]:
+			return maxLow(value,sortedList[0:midpoint])
+		else:
+			return midpoint
+
 	def sortedNumericList(self):
 		self.sortedNumList = sorted(self.numericValue.values())
 		return self.sortedNumList
@@ -28,40 +80,45 @@ class Attribute:
 		return self.cutList
 
 	def addCutPoint(self, cutValue):
-		self.cutpoints.append(CutPoint(self, cutValue))
-		self.cutpoints.sort(key=item.cutValue) #Look up syntax
-	#Selects best cutpoint for a single attribute	
-	def selectCutPoint(self):
-
-
-	class CutPoint:
-		def __init__(self, attribute, cutValue):
-			self.name = attrName
-			self.cutValue = cutValue
-			self.low = attribute.sortedNumList[0]
-			self.high = attribute.sortedNumList[-1]
-			self.span = "%02d..%02d" %(self.low, self.high)
+		self.cutpoints.append(cutValue)
+	
+	def partition(self):
+		inducedPartition = Partition()
+		if self.discreteValue:
+			for case, discValue in self.discreteValue.iteritems():
+				inducedPartition.addToElementalSet(case, discValue)
+		else:
+			for case, numValue in self.numericValue.iteritems():
+				inducedPartition.addToElementalSet(case, numValue)
+		return inducedPartition
 
 class DataSet:
-	class casePartition:
-		def __init__(self):
-			self.elementalSetList = list()
-			class elementalSet:
-				def __init__(self):
-					self.numberList = list()
-					
-
 	def __init__(self):
 		self.attributeList = list()
 
 	def partitionOfA(self):
+		partitionList = list()
+		for attr in self.attributeList[0:-2]:
+			partitionList.append(attr.partition())
+		while len(partitionList) > 1:
+			partitionList[0].mergePartitions(partitionList[-1])
+			partitionList.pop()
+		return partitionList[0]
 
 	def partitionOfDecision(self):
+		d = self.attributeList[-1]
+		partitionD = Partition()
+		for case, concept in d.discreteValue.iteritems():
+			partitionD.addToElementalSet(case, concept)
+		return partitionD
+
+
+
 
 	def kScans(self, k):
 
 	def isConsistent(self, partition1, partition2):
-		#for every elemental set in partition1, do the cases map to at most one set in partition2?
+		#for every elemental set in partition1, do the cases map to exactly one set in partition2?
 		return consistentBoolean
 
 	def condEntropy(self):
